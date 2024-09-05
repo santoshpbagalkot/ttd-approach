b329d4b8a6f6cf54da1cfe4303b9095ff1e034a5
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-09-05 17:19:10 +0530

    green phase >> minimal code to pass the testcases

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
 class Calculator:
     def add(self, numbers: str) -> int:
-        return NotImplementedError()
+        numbers_copy = numbers.strip()
+        if not numbers_copy:
+            return 0
+        numbers_array = numbers_copy.split(',')
+        if len(numbers_array) == 1:
+            return int(numbers_array[0])
+        else:
+            return (int(numbers_array[0]) + int(numbers_array[1]))
