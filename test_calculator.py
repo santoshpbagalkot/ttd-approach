9e90ba3159a7d690836f623a9f51c2c76415b3c1
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-09-05 16:10:14 +0530

    red phase

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,14 @@ class TestCalculatorAdd(unittest.TestCase):
     def setUp(self) -> None:
         """Set up the test fixtures."""
         pass
+    def test_empty_string_returns_zero(self):
+        calc_obj = Calculator()
+        self.assertEqual(calc_obj.add(""), 0)
+
+    def test_single_number_returns_the_number(self):
+        calc_obj = Calculator()
+        self.assertEqual(calc_obj.add("1"), 1)
+
+    def test_sum_of_two_numbers(self):
+        calc_obj = Calculator()
+        self.assertEqual(calc_obj.add("1,5"), 6)
