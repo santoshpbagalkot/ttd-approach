57dfd96c0b2ff5f09d8ba5296295f8ddc90c98d2
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-09-05 18:46:57 +0530

    refactor to allow custom delimiter pattern

--- calculator.py
+++ calculator.py
@@ -3,12 +3,28 @@ import re
 class Calculator:
 
     # Regular expression to split by comma or newline
-    pattern = r'[,\n]'
+    default_delimiter_pattern = r'[,\n]'
+    dymanic_delimiter_pattern = r'^//(.{1})\n'
+
+    def remove_pattern(self, pattern, text):
+        """
+        Utility to remove the delimiter pattern from the parent string to allow split on the captured delimiter
+        """
+        return re.sub(pattern, '', text)
+
 
     def add(self, numbers: str) -> int:
         numbers_copy = numbers.strip()
         if not numbers_copy:
             return 0
+        match = re.match(Calculator.dymanic_delimiter_pattern, numbers_copy)
+        if match:
+            # If we are supplied with a delimiter already, extract it to split the string on the same
+            delimiter = match.group(1) # Extract the delimiter from the () group
+            numbers_copy = self.remove_pattern(Calculator.dymanic_delimiter_pattern, numbers_copy)
+            numbers_copy = numbers_copy.split(delimiter)
+        else:
+            # If we are not supplied with a delimiter pattern use the default , or \n
+            numbers_copy = re.split(Calculator.default_delimiter_pattern, numbers_copy)
         # Split the string by commas, convert each part to int, and sum them up
-        numbers_copy = re.split(Calculator.pattern, numbers_copy)
         return sum(int(num) for num in numbers_copy if num.strip())
