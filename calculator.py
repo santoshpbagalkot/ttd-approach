fc27f5cd58d7db558eb031e9a28517b3822fbf64
santoshpbagalkot <santoshpbagalkot@gmail.com>
2024-09-05 17:58:01 +0530

    Refactor: Allow , or newline to be a separator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
+import re
+
 class Calculator:
+
+    # Regular expression to split by comma or newline
+    pattern = r'[,\n]'
+
     def add(self, numbers: str) -> int:
         numbers_copy = numbers.strip()
         if not numbers_copy:
             return 0
         # Split the string by commas, convert each part to int, and sum them up
-        return sum(int(num) for num in numbers_copy.split(',') if num.strip())
+        numbers_copy = re.split(Calculator.pattern, numbers_copy)
+        return sum(int(num) for num in numbers_copy if num.strip())
